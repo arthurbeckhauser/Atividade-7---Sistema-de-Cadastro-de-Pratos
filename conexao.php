3e06f1f5ff3891e1aaeaabd2c3937c68bc9f0e0b
arthurbeckhauser <arthur_b_fernandes@estudante.sesisenai.org.br>
2026-08-19 10:11:06 -0300

    add - porta php para o mysql

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-$conexao = new mysqli("localhost", "root", "", "restaurante");
+$conexao = new mysqli("localhost", "root", "", "restaurante", 6666);
 
 if ($conexao->connect_error) {
     die("Erro na conexão: " . $conexao->connect_error);
